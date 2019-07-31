107b0ad88feeee919757e78120e62c4a3040befe
yi-ting Chiang <tarynchiang@gmail.com>
2019-07-31 08:57:46 -0400

    finish user route

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const passport     = require('passport');
+const session      = require('express-session');
 
 
+require('./config/passport-stuff');
+
+mongoose.Promise = Promise;
 mongoose
   .connect('mongodb://localhost/project3-backend', {useNewUrlParser: true})
   .then(x => {
@@ -50,9 +55,28 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
+app.use(session({
+  secret:"some secret goes here",
+  resave: true,
+  saveUninitialized: true
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
+
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const userRoutes = require('./routes/userRoutes');
+app.use('/', userRoutes);
+
+
 
 
 module.exports = app;
